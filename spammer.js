e40e42691c87c31eb2c0b8affff5f45fb69da412
Daniel <brikbusters@gmail.com>
2020-06-30 19:03:01 +0100

    spammer.js

---
+++ spammer.js
@@ -0,0 +1,43 @@
+var chars = "1234567890QWERTYUIOASDFGHJKLPZXCVBNM";
+var websites = ["https://rbxpro.xyz/?confirmation="];
+var site = 0;
+var i = 0;
+
+function timeout(ms, promise) {
+	return new Promise(function(resolve, reject) {
+		setTimeout(function() {
+			reject(new Error("timeout"));
+		}, ms);
+
+		promise.then(resolve, reject);
+	});
+}
+
+function randomString() {
+	var randomString = "";
+	for (var c = 0; c < 360; c++) {
+		randomString += chars[Math.floor(Math.random() * chars.length)];
+	}
+
+	return randomString;
+}
+
+var requester;
+requester = function() {
+	timeout(2000, fetch(websites[site] + randomString())).then(result => {
+		console.log(i, result);
+		setTimeout(function() {
+			site++;
+			if (site == websites.length) {
+				site = 0;
+			}			
+
+			requester();
+		}, 100);
+		i++;
+	}).catch(function(error) {
+		console.log("Timeout error");
+	});
+}
+
+requester();
